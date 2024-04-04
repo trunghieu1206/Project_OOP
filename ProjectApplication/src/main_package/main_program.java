2d73540d170319c1a4efbb4a1003ccaa9dc83d98
Hieu Hoang <hieuhieuhieu12@gmail.com>
2024-04-04 20:03:04 +0700

    fix

--- ProjectApplication/src/main_package/main_program.java
+++ ProjectApplication/src/main_package/main_program.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class main_program {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello from individual-hieu branch");
 		
 	}
 }
