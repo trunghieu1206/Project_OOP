11176c0f52982c95e9c1aa2d71109eaec2df7258
Hieu Hoang <hieuhieuhieu12@gmail.com>
2024-05-10 17:41:03 +0700

    base

---
+++ ProjectApplication/src/main_package/VirusStorage/Membrane.java
@@ -0,0 +1,18 @@
+package main_package.VirusStorage;
+
+public class Membrane {
+	private int num;
+	
+	public Membrane(int num) {
+		this.num = num;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+}
