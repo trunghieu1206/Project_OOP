20a5f9ca1911a5d83ea5d35958a758c437828314
Hieu Hoang <hieuhieuhieu12@gmail.com>
2024-05-11 12:51:19 +0700

    Completed Virus, VirusWithoutLipidEnvelop, VirusWithLipidEnvelop class

--- ProjectApplication/src/main_package/App.java
+++ ProjectApplication/src/main_package/App.java
@@ -1,10 +1,5 @@
 package main_package;
 
-import javax.swing.*;
-
 public class App {
-	public static void main(String[] args) {
-		System.out.println("hello");
-		
-	}
+	
 }
--- ProjectApplication/src/main_package/VirusStorage/LipidEnvelop.java
+++ ProjectApplication/src/main_package/VirusStorage/LipidEnvelop.java
@@ -1,5 +1,17 @@
 package main_package.VirusStorage;
 
 public class LipidEnvelop {
+	private int num;
 	
+	public LipidEnvelop(int num) {
+		this.num = num;
+	}
+	
+	public int getNum() {
+		return this.num;
+	}
+	
+	public void setNum(int num) {
+		this.num = num;
+	}
 }
