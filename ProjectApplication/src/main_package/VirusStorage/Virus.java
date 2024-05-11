fce74dc25eeec8cc5c7dd10df84eb4b65661cb7b
Hieu Hoang <hieuhieuhieu12@gmail.com>
2024-05-11 09:52:16 +0700

    fix misnamed file

--- ProjectApplication/src/main_package/VirusStorage/Virus.java
+++ ProjectApplication/src/main_package/VirusStorage/Virus.java
@@ -1,7 +1,44 @@
 package main_package.VirusStorage;
 
 public abstract class Virus {
-	private AcidNucleic acidNucleic;
-	private Membrane membrane;
+	private AcidNucleic a;
+	private Membrane m;
+	private Nucleocapsid n;
+	private Glycoprotein g;
+	
+	public Virus(AcidNucleic a, Membrane m, Nucleocapsid n, Glycoprotein g) {
+		this.a = a;
+		this.m = m;
+		this.n = n;
+		this.g = g;
+	}
+	
+	public void entryIntoHostCell() {
+		
+	}
+	
+	public void uncoat() {
+		
+	}
+	
+	public void replicateGene() {
+		
+	}
+	
+	public void assemble() {
+		
+	}
+	
+	public void maturateAndRelease() {
+		
+	}
+	
+	public void infectToNewCells() {
+		
+	}
+	
+	public abstract void displayInformation();
+	
+	public abstract void attachToHostCell();
 	
 }
